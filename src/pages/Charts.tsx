44ff6d0d68317d8d7fb5f8e31d2bf9e3c9a71846
Jeremiah Towe <jtowe@foreground.co>
2022-03-16 12:18:52 -0400

    Use SprintProvider and context

--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSprint } from '../context/SprintContext';
 
 const Charts: React.FC = () => {
+    const sprint = useSprint();
+
     return (
         <>
+            <p>{sprint.name}</p>
             <p>Charts here</p>
         </>
     );
